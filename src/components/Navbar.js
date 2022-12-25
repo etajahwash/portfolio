3a8b2949fee32ad14fa91af82b18a6943972103c
E'Tajah Washington <etajahwash@gmail.com>
2022-12-25 10:57:28 -0500

    added menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,37 @@ import React from 'react'
 import '../styling/Navbar.css'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-scroll';
+import MenuIcon from '@mui/icons-material/Menu';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import {
+    Box,
+    Divider,
+    IconButton,
+    List,
+    ListItemButton,
+    ListItemText,
+  } from "@mui/material";
 
 export default function Navbar() {
     const [show, setShow] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+
+    function navProjects() {
+        setIsActive(false);
+      }
+    
+      function navAbout() {
+        setIsActive(false);
+      }
+    
+      function navContact() {
+        setIsActive(false);
+      }
+    
+      function navHome() {
+        setIsActive(false);
+      }
+
 
     function navbarTransition() {
         if(window.scrollY > 30) {
@@ -31,6 +59,55 @@ export default function Navbar() {
             <Link to="contact" spy={true} smooth={true} offset={0}>Contact Me</Link>
             </li>
         </ul>
+        <div className='menuSection'>
+        <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setIsActive(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SwipeableDrawer
+            anchor="right"
+            color="palevioletred"
+            open={isActive}
+            onClose={() => setIsActive(false)}
+            onOpen={() => {}}
+          >
+            <div
+              style={{
+                backgroundColor: "palevioletred",
+                height: "100vh",
+                width: "10rem",
+              }}
+            >
+              <Divider />
+              <List>
+              <Link to="landing" spy={true} smooth={true} offset={0}>
+                <ListItemButton onClick={navHome}>
+                    <ListItemText primary={"Home"} />
+                </ListItemButton>
+                </Link>
+                <Link to="about" spy={true} smooth={true} offset={0}>
+                <ListItemButton onClick={navAbout}>
+                  <ListItemText primary={"About Me"} />
+                </ListItemButton>
+                </Link>
+                <Link to="projects" spy={true} smooth={true} offset={0}>
+                <ListItemButton onClick={navProjects}>
+                  <ListItemText primary={"My Projects"} />
+                </ListItemButton>
+                </Link>
+                <Link to="contact" spy={true} smooth={true} offset={0}>
+                <ListItemButton onClick={navContact}>
+                  <ListItemText primary={"Contact Me"} />
+                </ListItemButton>
+                </Link>
+              </List>
+            </div>
+          </SwipeableDrawer>
+        </div>
     </div>
   )
 }
